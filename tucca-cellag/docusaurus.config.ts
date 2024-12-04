a9e84216651a87f2ad655881552632a349021c43
benjibromberg <bromberg.benji@gmail.com>
2024-12-04 12:10:42 -0500

    modify deploymentBranch

--- tucca-cellag/docusaurus.config.ts
+++ tucca-cellag/docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tucca-cellag', // Usually your GitHub org/user name.
   projectName: 'tucca-cellag.github.io', // Usually your repo name.
-  deploymentBranch: '',
+  deploymentBranch: 'latest',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
