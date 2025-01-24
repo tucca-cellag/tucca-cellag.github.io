0f966bfa9c7629e8fad05b9ed56b0df2d5f457d1
benjibromberg <bromberg.benji@gmail.com>
2025-01-24 14:20:25 -0500

    Homepage updates

--- tucca-cellag/src/pages/index.tsx
+++ tucca-cellag/src/pages/index.tsx
@@ -74,8 +74,35 @@ const Home: React.FC = () => {
         <section
           className={clsx("section section--primary", styles.cardSection)}
         >
-          <div className="container">
+          <div className="container text--center">
+            <h1> About Us </h1>
             <Columns>
+              <Column className="text--left">
+                <h2> What is TUCCA? </h2>
+                <p style={{ marginBottom: "20px" }}>
+                  The{" "}
+                  <strong>
+                    Tufts University Center for Cellular Agriculture (TUCCA)
+                  </strong>{" "}
+                  is dedicated to advancing cellular agriculture research and
+                  fostering industry growth. Collaborating with academic,
+                  industry, and regulatory partners, TUCCA focuses on innovative
+                  research in areas such as cell sourcing, media development,
+                  scaffolding, modeling, and scale-up processes. Our goal is to
+                  provide a strong scientific foundation, promote workforce
+                  development, and ensure the sustainability and safety of
+                  cell-based foods. Through peer-reviewed publications and
+                  interdisciplinary efforts, we aim to drive technological
+                  innovation and set quality standards within the field.
+                </p>
+                <div className="btn-group">
+                  <Button
+                    label="Learn More"
+                    link="https://cellularagriculture.tufts.edu/"
+                    variant="primary"
+                  />
+                </div>
+              </Column>
               <Column className="text--center">
                 <div data-aos="fade-right" data-aos-duration="2500">
                   <Card shadow="tl">
@@ -83,22 +110,38 @@ const Home: React.FC = () => {
                       <h2>About Cellular Agriculture 🥩</h2>
                     </CardHeader>
                     <CardBody className="text--left">
-                      Cellular Agriculture leverages biotechnology to produce
-                      agricultural products directly from cells, offering
-                      sustainable and ethical alternatives to traditional
-                      farming.
+                      Cellular Agriculture utilizes cutting-edge biotechnology
+                      to cultivate agricultural products directly from cells
+                      instead of relying on traditional farming methods. This
+                      innovative approach focuses on growing meat, dairy, and
+                      other animal-based products in a lab environment,
+                      significantly reducing the environmental impact and
+                      ethical concerns associated with conventional agriculture.
+                      As a rapidly evolving field, cellular agriculture holds
+                      the promise of revolutionizing how we produce food, paving
+                      the way for a more sustainable and humane future.
                     </CardBody>
                     <CardFooter>
                       <Button
                         className="button--block"
-                        label="Learn More"
-                        link="https://cellularagriculture.tufts.edu/"
+                        label="Check Out Our Research"
+                        link="https://cellularagriculture.tufts.edu/research"
                         variant="primary"
                       />
                     </CardFooter>
                   </Card>
                 </div>
               </Column>
+            </Columns>
+          </div>
+        </section>
+
+        <section
+          className={clsx("section section--primary", styles.cardSection)}
+        >
+          <div className="container text--center">
+            <h1> Our Work </h1>
+            <Columns>
               <Column className="text--center">
                 <div data-aos="fade-right" data-aos-duration="2000">
                   <Card shadow="tl">
